a9a9fd4051514630d891c462becb2bad7390377f
yusif <yusif.aliyev.25@mail.ru>
2024-01-08 22:21:41 +0400

    async function

--- main.js
+++ main.js
@@ -12,35 +12,51 @@ srcLine.addEventListener("keypress", event =>{
     }
 })
 
-srcBtn.addEventListener("click", function(){
-    const movieTitle = srcLine.value;
-    console.log(movieTitle);
-const myPromise = fetch(`https://www.omdbapi.com/?apikey=a407a7b3&s=${movieTitle}`);
-myPromise.then(res =>{
-    const promise = res.json();
+// const myPromise = fetch(`https://www.omdbapi.com/?apikey=a407a7b3&s=${movieTitle}`);
+// myPromise.then(res =>{
+    //     const promise = res.json();
+    
+    //     return promise
+// }).then(rederItem).catch(err=>{
+    //     console.log(err);
+// }).finally(()=>{
+//     console.log("The End");
 
-    return promise
-}).then(rederItem).catch(err=>{
-    console.log(err);
-}).finally(()=>{
-    console.log("The End");
+srcBtn.addEventListener("click", function(){
+    const title = srcLine.value;
+    // console.log(movieTitle);
+
+    myPromise(title)
 });
+
+async function myPromise(movieTitle){
+    
+    try{
+        let url = `https://www.omdbapi.com/?apikey=a407a7b3&s=${movieTitle}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        rederItem(data);
+    }catch(err){
+        console.log(err);
+    }
+}
+
 
 function rederItem(data){
     const movie = data.Search
     console.log(movie);
-card.innerHTML = movie.map(item => `<div class="card " style="width: 18rem;">
-<img src="${item.Poster}" class="card-img-top" alt="...">
-<div class="card-body">
-  <h5 class="card-title">${item.Title}</h5>
-  <p class="card-text">${item.Year}</p>
-  <p class="card-text">${item.Type}</p>
-  <a href="#" class="btn btn-primary">${item.imdbID}</a>
-</div>
-</div>`)
+    card.innerHTML = movie.map(item => `<div class="card " style="width: 18rem;">
+    <img src="${item.Poster}" class="card-img-top" alt="...">
+    <div class="card-body">
+    <h5 class="card-title">${item.Title}</h5>
+    <p class="card-text">${item.Year}</p>
+    <p class="card-text">${item.Type}</p>
+    <a href="#" class="btn btn-primary">${item.imdbID}</a>
+    </div>
+    </div>`)
+  
 
+    srcLine.value = "";
 }
 
 
-    srcLine.value = "";
-})
